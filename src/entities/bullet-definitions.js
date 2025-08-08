6858890651738c3196b41f104c29c89c86681270
Victor Wu <victorlwu0512@gmail.com>
2025-08-08 06:54:14 -0600

    Bullet changes

--- src/entities/bullet-definitions.js
+++ src/entities/bullet-definitions.js
@@ -2,19 +2,19 @@ export const BULLET_CONFIG = {
     fire_bullet: {
         speed: 800,       // pixels per second
         lifespan: 1.5,    // seconds
-        animationName: "anim_0_0", // Top-leftmost animation
+        animationName: "anim_0_4", // Top-leftmost animation
         assetKey: "Red",
     },
     ice_bullet: {
         speed: 600,
         lifespan: 2.0,
-        animationName: "anim_0_0",
+        animationName: "anim_2_4",
         assetKey: "Blue",
     },
     lightning_bullet: {
         speed: 1000,
         lifespan: 1.0,
-        animationName: "anim_0_0",
+        animationName: "anim_8_0",
         assetKey: "Yellow",
     },
 };
