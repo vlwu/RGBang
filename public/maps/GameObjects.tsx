9e2e2f9bd92c7188531f66363e519653b134f5c5
Victor Wu <victorlwu0512@gmail.com>
2025-08-08 17:46:21 -0600

    Setup for objects

---
+++ public/maps/GameObjects.tsx
@@ -0,0 +1,244 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="GameObjects" tilewidth="128" tileheight="128" tilecount="80" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Autumn_tree1.png" width="128" height="128"/>
+ </tile>
+ <tile id="1">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Autumn_tree2.png" width="64" height="64"/>
+ </tile>
+ <tile id="2">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Autumn_tree3.png" width="64" height="64"/>
+ </tile>
+ <tile id="3">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Broken_tree1.png" width="32" height="32"/>
+ </tile>
+ <tile id="4">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Broken_tree2.png" width="32" height="32"/>
+ </tile>
+ <tile id="5">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Broken_tree3.png" width="48" height="48"/>
+ </tile>
+ <tile id="6">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Broken_tree4.png" width="64" height="64"/>
+ </tile>
+ <tile id="7">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Broken_tree5.png" width="32" height="32"/>
+ </tile>
+ <tile id="8">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Broken_tree6.png" width="16" height="16"/>
+ </tile>
+ <tile id="9">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Broken_tree7.png" width="128" height="128"/>
+ </tile>
+ <tile id="10">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Burned_tree1.png" width="128" height="128"/>
+ </tile>
+ <tile id="11">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Burned_tree2.png" width="64" height="64"/>
+ </tile>
+ <tile id="12">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Burned_tree3.png" width="64" height="64"/>
+ </tile>
+ <tile id="13">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Christmas_tree1.png" width="128" height="128"/>
+ </tile>
+ <tile id="14">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Christmas_tree2.png" width="64" height="64"/>
+ </tile>
+ <tile id="15">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Christmas_tree3.png" width="32" height="32"/>
+ </tile>
+ <tile id="16">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Flower_tree1.png" width="128" height="128"/>
+ </tile>
+ <tile id="17">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Flower_tree2.png" width="64" height="64"/>
+ </tile>
+ <tile id="18">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Flower_tree2-1.png" width="64" height="64"/>
+ </tile>
+ <tile id="19">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Fruit_tree1.png" width="128" height="128"/>
+ </tile>
+ <tile id="20">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Fruit_tree2.png" width="128" height="128"/>
+ </tile>
+ <tile id="21">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Fruit_tree3.png" width="64" height="64"/>
+ </tile>
+ <tile id="22">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Moss_tree1.png" width="128" height="128"/>
+ </tile>
+ <tile id="23">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Moss_tree2.png" width="64" height="64"/>
+ </tile>
+ <tile id="24">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Moss_tree3.png" width="64" height="64"/>
+ </tile>
+ <tile id="25">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Palm_tree1_1.png" width="128" height="128"/>
+ </tile>
+ <tile id="26">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Palm_tree1_2.png" width="64" height="64"/>
+ </tile>
+ <tile id="27">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Palm_tree1_3.png" width="64" height="64"/>
+ </tile>
+ <tile id="28">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Palm_tree2_1.png" width="128" height="128"/>
+ </tile>
+ <tile id="29">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Palm_tree2_2.png" width="64" height="64"/>
+ </tile>
+ <tile id="30">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Palm_tree2_3.png" width="64" height="64"/>
+ </tile>
+ <tile id="31">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Snow_christmass_tree1.png" width="128" height="128"/>
+ </tile>
+ <tile id="32">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Snow_christmass_tree2.png" width="64" height="64"/>
+ </tile>
+ <tile id="33">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Snow_christmass_tree3.png" width="32" height="32"/>
+ </tile>
+ <tile id="34">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Snow_tree1.png" width="128" height="128"/>
+ </tile>
+ <tile id="35">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Snow_tree2.png" width="64" height="64"/>
+ </tile>
+ <tile id="36">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Snow_tree3.png" width="64" height="64"/>
+ </tile>
+ <tile id="37">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Tree1.png" width="128" height="128"/>
+ </tile>
+ <tile id="38">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Tree2.png" width="64" height="64"/>
+ </tile>
+ <tile id="39">
+  <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Tree3.png" width="64" height="64"/>
+ </tile>
+ <tile id="40">
+  <image source="../images/terrain/crystals/Assets_shadow/Black_crystal1.png" width="64" height="64"/>
+ </tile>
+ <tile id="41">
+  <image source="../images/terrain/crystals/Assets_shadow/Black_crystal2.png" width="64" height="64"/>
+ </tile>
+ <tile id="42">
+  <image source="../images/terrain/crystals/Assets_shadow/Black_crystal3.png" width="32" height="32"/>
+ </tile>
+ <tile id="43">
+  <image source="../images/terrain/crystals/Assets_shadow/Black_crystal4.png" width="16" height="16"/>
+ </tile>
+ <tile id="44">
+  <image source="../images/terrain/crystals/Assets_shadow/Blue_crystal1.png" width="64" height="64"/>
+ </tile>
+ <tile id="45">
+  <image source="../images/terrain/crystals/Assets_shadow/Blue_crystal2.png" width="64" height="64"/>
+ </tile>
+ <tile id="46">
+  <image source="../images/terrain/crystals/Assets_shadow/Blue_crystal3.png" width="32" height="32"/>
+ </tile>
+ <tile id="47">
+  <image source="../images/terrain/crystals/Assets_shadow/Blue_crystal4.png" width="16" height="16"/>
+ </tile>
+ <tile id="48">
+  <image source="../images/terrain/crystals/Assets_shadow/Dark_red_ crystal1.png" width="64" height="64"/>
+ </tile>
+ <tile id="49">
+  <image source="../images/terrain/crystals/Assets_shadow/Dark_red_ crystal2.png" width="64" height="64"/>
+ </tile>
+ <tile id="50">
+  <image source="../images/terrain/crystals/Assets_shadow/Dark_red_ crystal3.png" width="32" height="32"/>
+ </tile>
+ <tile id="51">
+  <image source="../images/terrain/crystals/Assets_shadow/Dark_red_ crystal4.png" width="16" height="16"/>
+ </tile>
+ <tile id="52">
+  <image source="../images/terrain/crystals/Assets_shadow/Green_crystal1.png" width="64" height="64"/>
+ </tile>
+ <tile id="53">
+  <image source="../images/terrain/crystals/Assets_shadow/Green_crystal2.png" width="64" height="64"/>
+ </tile>
+ <tile id="54">
+  <image source="../images/terrain/crystals/Assets_shadow/Green_crystal3.png" width="32" height="32"/>
+ </tile>
+ <tile id="55">
+  <image source="../images/terrain/crystals/Assets_shadow/Green_crystal4.png" width="16" height="16"/>
+ </tile>
+ <tile id="56">
+  <image source="../images/terrain/crystals/Assets_shadow/Pink_crystal1.png" width="64" height="64"/>
+ </tile>
+ <tile id="57">
+  <image source="../images/terrain/crystals/Assets_shadow/Pink_crystal2.png" width="64" height="64"/>
+ </tile>
+ <tile id="58">
+  <image source="../images/terrain/crystals/Assets_shadow/Pink_crystal3.png" width="32" height="32"/>
+ </tile>
+ <tile id="59">
+  <image source="../images/terrain/crystals/Assets_shadow/Pink_crystal4.png" width="16" height="16"/>
+ </tile>
+ <tile id="60">
+  <image source="../images/terrain/crystals/Assets_shadow/Red_crystal1.png" width="64" height="64"/>
+ </tile>
+ <tile id="61">
+  <image source="../images/terrain/crystals/Assets_shadow/Red_crystal2.png" width="64" height="64"/>
+ </tile>
+ <tile id="62">
+  <image source="../images/terrain/crystals/Assets_shadow/Red_crystal3.png" width="32" height="32"/>
+ </tile>
+ <tile id="63">
+  <image source="../images/terrain/crystals/Assets_shadow/Red_crystal4.png" width="16" height="16"/>
+ </tile>
+ <tile id="64">
+  <image source="../images/terrain/crystals/Assets_shadow/Violet_crystal1.png" width="64" height="64"/>
+ </tile>
+ <tile id="65">
+  <image source="../images/terrain/crystals/Assets_shadow/Violet_crystal2.png" width="64" height="64"/>
+ </tile>
+ <tile id="66">
+  <image source="../images/terrain/crystals/Assets_shadow/Violet_crystal3.png" width="32" height="32"/>
+ </tile>
+ <tile id="67">
+  <image source="../images/terrain/crystals/Assets_shadow/Violet_crystal4.png" width="16" height="16"/>
+ </tile>
+ <tile id="68">
+  <image source="../images/terrain/crystals/Assets_shadow/White_crystal1.png" width="64" height="64"/>
+ </tile>
+ <tile id="69">
+  <image source="../images/terrain/crystals/Assets_shadow/White_crystal2.png" width="64" height="64"/>
+ </tile>
+ <tile id="70">
+  <image source="../images/terrain/crystals/Assets_shadow/White_crystal3.png" width="32" height="32"/>
+ </tile>
+ <tile id="71">
+  <image source="../images/terrain/crystals/Assets_shadow/White_crystal4.png" width="16" height="16"/>
+ </tile>
+ <tile id="72">
+  <image source="../images/terrain/crystals/Assets_shadow/Yellow_crystal1.png" width="64" height="64"/>
+ </tile>
+ <tile id="73">
+  <image source="../images/terrain/crystals/Assets_shadow/Yellow_crystal2.png" width="64" height="64"/>
+ </tile>
+ <tile id="74">
+  <image source="../images/terrain/crystals/Assets_shadow/Yellow_crystal3.png" width="32" height="32"/>
+ </tile>
+ <tile id="75">
+  <image source="../images/terrain/crystals/Assets_shadow/Yellow_crystal4.png" width="16" height="16"/>
+ </tile>
+ <tile id="76">
+  <image source="../images/terrain/crystals/Assets_shadow/Yellow-green_crystal1.png" width="64" height="64"/>
+ </tile>
+ <tile id="77">
+  <image source="../images/terrain/crystals/Assets_shadow/Yellow-green_crystal2.png" width="64" height="64"/>
+ </tile>
+ <tile id="78">
+  <image source="../images/terrain/crystals/Assets_shadow/Yellow-green_crystal3.png" width="32" height="32"/>
+ </tile>
+ <tile id="79">
+  <image source="../images/terrain/crystals/Assets_shadow/Yellow-green_crystal4.png" width="16" height="16"/>
+ </tile>
+</tileset>
