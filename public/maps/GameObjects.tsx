0c79b52e71cb9b6f475c2d7fb6ff7b103da5a2a6
Victor Wu <victorlwu0512@gmail.com>
2025-08-08 21:00:10 -0600

    Added collision hitboxes

--- public/maps/GameObjects.tsx
+++ public/maps/GameObjects.tsx
@@ -9,6 +9,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Autumn_tree1.png" width="128" height="128"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="45" y="83.6667" width="33.6667" height="18">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="81" type="GameObject">
   <properties>
@@ -18,6 +23,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Autumn_tree2.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="47.6667" width="29" height="16.3333">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="82" type="GameObject">
   <properties>
@@ -27,6 +37,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Autumn_tree3.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="20.6667" y="40.3333" width="21.6667" height="13.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="83" type="GameObject">
   <properties>
@@ -36,6 +51,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Broken_tree1.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="21">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="84" type="GameObject">
   <properties>
@@ -45,6 +65,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Broken_tree2.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.6667" y="19.3333">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="85" type="GameObject">
   <properties>
@@ -54,6 +79,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Broken_tree3.png" width="48" height="48"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7" y="16.3333" width="32.6667" height="19.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="86" type="GameObject">
   <properties>
@@ -63,6 +93,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Broken_tree4.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="21" y="26.6667" width="25" height="14.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="87" type="GameObject">
   <properties>
@@ -72,6 +107,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Broken_tree5.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="14.3333" y="17.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="88" type="GameObject">
   <properties>
@@ -81,6 +121,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Broken_tree6.png" width="16" height="16"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.66667" y="4.33333" width="11.3333" height="7">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="89" type="GameObject">
   <properties>
@@ -90,6 +135,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Broken_tree7.png" width="128" height="128"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="33.6667" y="60" width="55.3333" height="15">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="90" type="GameObject">
   <properties>
@@ -99,6 +149,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Burned_tree1.png" width="128" height="128"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="48.6667" y="78.3333" width="26.3333" height="20.3333">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="91" type="GameObject">
   <properties>
@@ -108,6 +163,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Burned_tree2.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="30" y="52.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="92" type="GameObject">
   <properties>
@@ -117,6 +177,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Burned_tree3.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="23.6667" y="38.3333" width="19.3333" height="11.3333">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="93" type="GameObject">
   <properties>
@@ -126,6 +191,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Christmas_tree1.png" width="128" height="128"/>
+  <objectgroup draworder="index" id="3">
+   <object id="3" x="46.6667" y="83.6667" width="35.6667" height="18.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="94" type="GameObject">
   <properties>
@@ -135,6 +205,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Christmas_tree2.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="20.6667" y="44.3333" width="23.3333" height="12.3333">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="95" type="GameObject">
   <properties>
@@ -144,6 +219,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Christmas_tree3.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7.33333" y="15.6667" width="18" height="12">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="96" type="GameObject">
   <properties>
@@ -153,6 +233,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Flower_tree1.png" width="128" height="128"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="62.3333" y="87.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="97" type="GameObject">
   <properties>
@@ -162,6 +247,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Flower_tree2.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="30.3333" y="52.3333">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="98" type="GameObject">
   <properties>
@@ -171,6 +261,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Flower_tree2-1.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24.3333" y="37.3333" width="16" height="10.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="99" type="GameObject">
   <properties>
@@ -180,6 +275,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Fruit_tree1.png" width="128" height="128"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="62" y="89.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="100" type="GameObject">
   <properties>
@@ -189,6 +289,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Fruit_tree2.png" width="128" height="128"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="63.6667" y="85">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="101" type="GameObject">
   <properties>
@@ -198,6 +303,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Fruit_tree3.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="25" y="38" width="15.3333" height="12.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="102" type="GameObject">
   <properties>
@@ -207,6 +317,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Moss_tree1.png" width="128" height="128"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="49" y="83" width="28" height="17.3333">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="103" type="GameObject">
   <properties>
@@ -216,6 +331,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Moss_tree2.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="33.6667" y="51.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="104" type="GameObject">
   <properties>
@@ -225,6 +345,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Moss_tree3.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="23.6667" y="38.6667" width="18" height="12.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="105" type="GameObject">
   <properties>
@@ -234,6 +359,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Palm_tree1_1.png" width="128" height="128"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="63.6667" y="88.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="106" type="GameObject">
   <properties>
@@ -243,6 +373,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Palm_tree1_2.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32.3333" y="53">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="107" type="GameObject">
   <properties>
@@ -252,6 +387,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Palm_tree1_3.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24.6667" y="36.3333" width="16.6667" height="12.3333">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="108" type="GameObject">
   <properties>
@@ -261,6 +401,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Palm_tree2_1.png" width="128" height="128"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="64.6667" y="88">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="109" type="GameObject">
   <properties>
@@ -270,6 +415,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Palm_tree2_2.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24.3333" y="43" width="16" height="16.3333">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="110" type="GameObject">
   <properties>
@@ -279,6 +429,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Palm_tree2_3.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="26" y="35.6667" width="14" height="12.3333">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="111" type="GameObject">
   <properties>
@@ -287,6 +442,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Snow_christmass_tree1.png" width="128" height="128"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="49.6667" y="81" width="26.6667" height="21.3333">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="112" type="GameObject">
   <properties>
@@ -295,6 +455,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Snow_christmass_tree2.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32.6667" y="50">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="113" type="GameObject">
   <properties>
@@ -303,6 +468,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Snow_christmass_tree3.png" width="32" height="32"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6.66667" y="16.3333" width="17.6667" height="11.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="114" type="GameObject">
   <properties>
@@ -312,6 +482,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Snow_tree1.png" width="128" height="128"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="50" y="80.3333" width="23.3333" height="17.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="115" type="GameObject">
   <properties>
@@ -321,6 +496,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Snow_tree2.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="52.3333">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="116" type="GameObject">
   <properties>
@@ -330,6 +510,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Snow_tree3.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="22.6667" y="39" width="18" height="13.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="117" type="GameObject">
   <properties>
@@ -339,6 +524,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Tree1.png" width="128" height="128"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="52" y="81" width="25.3333" height="20">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="118" type="GameObject">
   <properties>
@@ -348,6 +538,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Tree2.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="31.6667" y="53.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="119" type="GameObject">
   <properties>
@@ -357,6 +552,11 @@
    <property name="type" value="Tree"/>
   </properties>
   <image source="../images/terrain/trees/Assets Separate/Trees_shadow/Tree3.png" width="64" height="64"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="23" y="37.6667" width="19" height="15.6667">
+    <ellipse/>
+   </object>
+  </objectgroup>
  </tile>
  <tile id="120" type="GameObject">
   <properties>
