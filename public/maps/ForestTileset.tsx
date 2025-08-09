9f306311fb4129c73bebeb62709d425ee947c849
Victor Wu <victorlwu0512@gmail.com>
2025-08-08 19:30:46 -0600

    Forest tileset

---
+++ public/maps/ForestTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="ForestTileset" tilewidth="16" tileheight="16" tilecount="992" columns="32">
+ <image source="../images/terrain/biomes/forest_/forest_tileset.png" trans="ff00ff" width="512" height="496"/>
+</tileset>
