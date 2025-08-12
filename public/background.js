ff3f804437239a3808ed8590dd7f69fe22f5455f
Victor Wu <victorlwu0512@gmail.com>
2025-08-11 20:54:32 -0600

    Revert "When I launch the "Load unpacked" out/ folder, it seems the popup is inc"
    This reverts commit 4b10221b2262858c3053e74b5501c257a33775ea.

--- public/background.js
+++ public/background.js
@@ -1,6 +1,5 @@
-
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.create({
-    url: 'index.html'
-  });
+// This script can be expanded for more complex extension logic.
+// For now, it's just here to satisfy the manifest requirement.
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('RGBang extension installed.');
 });
