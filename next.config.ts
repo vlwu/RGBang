4e9060a78e821139b713a219db71c1cf1bf747da
Victor Wu <victorlwu0512@gmail.com>
2025-08-13 07:00:14 -0600

    Update next.config.js

--- next.config.ts
+++ next.config.ts
@@ -13,14 +13,6 @@ const nextConfig: NextConfig = {
   },
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
 };
 
